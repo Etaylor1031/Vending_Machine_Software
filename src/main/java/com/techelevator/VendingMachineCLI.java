86a8fb23204b9ca5b76627f3233c0b4618eab897
Ian <iancpatricio@gmail.com>
2023-04-16 13:01:46 -0700

    added throws IOException to methods

--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -2,14 +2,16 @@ package com.techelevator;
 
 import com.techelevator.view.Menu;
 import com.techelevator.view.VendingMachineInterface;
+import com.techelevator.view.VendingMachineSalesReport;
+
+import java.io.IOException;
 
 public class VendingMachineCLI {
-
 	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
-
 	private static final String MAIN_MENU_OPTION_EXIT = "Exit";
-	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE, MAIN_MENU_OPTION_EXIT };
+	private static final String MAIN_MENU_OPTION_SALES_REPORT = "Sales Report";
+	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE, MAIN_MENU_OPTION_SALES_REPORT };
 
 	private Menu menu;
 
@@ -17,10 +19,9 @@ public class VendingMachineCLI {
 		this.menu = menu;
 	}
 
-	public void run() {
+	public void run() throws IOException {
 		// Restock the Vending Machine
 		VendingMachineInterface vendingMachine = new VendingMachineInterface(System.in, System.out);
-
 		while (true) {
 			String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
 
@@ -32,11 +33,14 @@ public class VendingMachineCLI {
 				vendingMachine.purchase();
 			} else if (choice.equals(MAIN_MENU_OPTION_EXIT)) {
 				break;
+			} else if (choice.equals(MAIN_MENU_OPTION_SALES_REPORT)) {
+
 			}
+
 		}
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Menu menu = new Menu(System.in, System.out);
 		VendingMachineCLI cli = new VendingMachineCLI(menu);
 		cli.run();
